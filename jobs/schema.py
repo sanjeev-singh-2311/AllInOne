8c4dcaebfba4290adce8564c4e2ab704eb66150b
Sanjeev Singh <sanjeev.2311.singh@gmail.com>
2024-05-29 11:39:16 +0530

    [schema.py] Added JSON schema

---
+++ jobs/schema.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import List
+from pydantic import BaseModel, Field
+
+
+class JobCreate(BaseModel):
+
+    name: str = Field(description="Name of the worker")
+    experience: str = Field(description="Experience -> 1Y, 3Y, 10Y+ etc")
+    expertise: str = Field(description="The type of job expertise ")
+
+    cost_lower: float = Field(description="The lower limit of charges", gt=0)
+    cost_upper: float = Field(description="The upper limit of charges", gt=0)
+
+    time_slots: List[datetime] = Field(
+        description="All the Time slots with date and time"
+    )
+
+
+class JobInfo(JobCreate):
+    rating: float = Field(description="Rating out of 5", gt=0.0, lt=5.0)
+
+    availabe_time_slots: List[datetime] = Field(
+        description="Available Time slots with date and time"
+    )
+
+
+class Job(JobInfo):
+    id: int = Field(description="Job ID for unique identification, AUTO assigned")
